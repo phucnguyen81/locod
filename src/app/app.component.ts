507eb7eb57132eaf41c876189d2344237f81ee68
phucknguyen <phucknguyen@gmail.com>
2021-12-11 07:02:35 +0700

    Paste from clipboard periodically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatInput } from '@angular/material/input';
 
@@ -7,10 +7,33 @@ import { MatInput } from '@angular/material/input';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'locod';
 
+  tickDuration = 1000;
+
   textControl = new FormControl('');
+
+  pasteIntervalID = 0;
+
+  ngOnInit(): void {
+    this.startPaste();
+  }
+
+  ngOnDestroy(): void {
+    this.stopPaste();
+  }
+
+  // Read from clipboard periodically and write to textarea
+  startPaste(): void {
+    const paste = () => this.paste(new Event('custompaste'));
+    this.pasteIntervalID = window.setInterval(paste, 3 * this.tickDuration);
+  }
+
+  // Cancel the paste calls
+  stopPaste() {
+    window.clearInterval(this.pasteIntervalID);
+  }
 
   // Read from textarea and write to clipboard
   copy(event: Event): void {
@@ -22,21 +45,22 @@ export class AppComponent {
         console.log('Text copied');
       })
       .catch((error) => {
-        alert('Failed to write to clipboard: ' + String(error));
+        console.log('Failed to write to clipboard: ' + String(error));
       });
   }
 
   // Read from clipboard and write to textarea
   paste(event: Event): void {
     event.preventDefault();
-    navigator.clipboard
-      .readText()
-      .then((text: string) => {
-        this.textControl.setValue(text);
-        console.log('Pasted: ' + text);
-      })
-      .catch((error: any) => {
-        alert('Failed to read from clipboard: ' + String(error));
-      });
+    if (document.hasFocus()) {
+      navigator.clipboard
+        .readText()
+        .then((text: string) => {
+          this.textControl.setValue(text);
+        })
+        .catch((error: any) => {
+          console.log('Failed to read from clipboard: ' + String(error));
+        });
+    }
   }
 }
