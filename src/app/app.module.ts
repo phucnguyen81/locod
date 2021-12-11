29ebb1e5c2d7ab36c9eb8d95eeb6f9a60548daf7
phucknguyen <phucknguyen@gmail.com>
2021-12-11 14:12:59 +0700

    Use material Card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { MatSliderModule } from '@angular/material/slider';
-import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatListModule } from '@angular/material/list';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule,
-    MatSliderModule,
-    MatInputModule,
+    BrowserModule,
     MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatInputModule,
+    MatListModule,
+    MatSliderModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
