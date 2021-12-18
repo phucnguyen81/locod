2a9f1d17cf373e16fe3d810aa8ec0c552fe817b2
phucknguyen <phucknguyen@gmail.com>
2021-12-18 17:43:08 +0700

    Replace Card with List for a more compact view

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSliderModule } from '@angular/material/slider';
@@ -22,6 +23,7 @@ import { AppComponent } from './app.component';
     MatButtonModule,
     MatCardModule,
     MatDividerModule,
+    MatIconModule,
     MatInputModule,
     MatListModule,
     MatSliderModule,
